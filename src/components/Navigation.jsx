27d611df8d1634dff4d54f44512d2a467c6ac7eb
Jay <jjayakumar@computer-talk.com>
2020-08-18 21:23:05 -0400

    Added Navigation and Logo components as well as their respective styling

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from "react"; 
+
+function Navigation () {
+
+    return (
+       <div style ={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p className = 'f3 link dim black underline pa3 pointer'> Sign Out </p>
+       </div>
+    );
+
+}
+
+export default Navigation; 
